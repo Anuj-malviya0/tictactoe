a80e18f9ffaf5a60a33e622502ec198c045e422a
Anuj Malviya <75200824+Anuj-malviya0@users.noreply.github.com>
2021-02-06 21:43:47 +0530

    Added main file

---
+++ tictactoe.py
@@ -0,0 +1,29 @@
+#Creating a function to display board 
+def display_board(board):
+    clear_output()  # Remember, this only works in jupyter!
+    
+    print('   |   |')
+    print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
+    print('   |   |')
+    print('-----------')
+    print('   |   |')
+    print(' ' + board[4] + ' | ' + board[5] + ' | ' + board[6])
+    print('   |   |')
+    print('-----------')
+    print('   |   |')
+    print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
+    print('   |   |')
+#Creating a function to for selecting a marker (X or O)
+def player_input():
+    marker = ''
+    
+    while not (marker == 'X' or marker == 'O'):
+        marker = input('Player 1: Do you want to be X or O? ').upper()
+
+    if marker == 'X':
+        return ('X', 'O')
+    else:
+        return ('O', 'X')
+#Creating a function to place marker on the board 
+def place_marker(board, marker, position):
+    board[position] = marker
